558b19f799bc156908acee7da6333a4d738e788c
Sujoy B <70602535+Sujoy-Barua@users.noreply.github.com>
2024-06-08 04:24:42 -0400

    Added a login page connected to google firebase

---
+++ firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD1XfKt-bTLkMBk4hNgqXHR0DC4UJWIZLw",
+  authDomain: "uber-clone-ee6d9.firebaseapp.com",
+  projectId: "uber-clone-ee6d9",
+  storageBucket: "uber-clone-ee6d9.appspot.com",
+  messagingSenderId: "391649102542",
+  appId: "1:391649102542:web:1ea31a6e4998f0b991de9f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider()
+const auth = getAuth()
+
+export { app, provider, auth }
